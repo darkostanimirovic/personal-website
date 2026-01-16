317c503dbf4c0a72dc7b43973ef0877f39bf8e6b
Darko Stanimirovic <dstanimirovic@ohalo.co>
2026-01-16 18:11:46 +0100

    Turned off responsive -- annoying!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata, Viewport } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Darko Stanimirović - Product Manager',
-  description: 'Official homepage of Darko Stanimirović - AI Product Manager',
-}
+  title: "Darko Stanimirović - Product Manager",
+  description: "Official homepage of Darko Stanimirović - AI Product Manager",
+};
+
+export const viewport: Viewport = {
+  width: 800,
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
